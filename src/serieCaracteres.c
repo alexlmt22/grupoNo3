fc274efa5fba6189e75f36f264507a2170a11e4b
Leleglosh <stephanielozada42@yahoo.com>
2025-05-24 13:52:32 -0500

    Serie S1 Caracter

--- src/serieCaracteres.c
+++ src/serieCaracteres.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 
-void llSerieS1CFor (int llnumero)
+void llSerieS1CFor(int llCantidad)
 {
-    for (int i = 1; i <= llnumero; i++)
+    printf("\nSerie S1C For:\n");
+    for (int i = 0; i <= llCantidad; i++)
     {
-        if (i % 2 == 0)
-        {
-            printf("%c ", "+ ");
-        }
-        else
-        {
-            printf("%c ", "- ");
-        }
+         printf("%c ", (i % 2 == 0) ? '+' : '-');
     }
     printf("\n");
 }
 
+void llSerieS1CWhile(int llCantidad)
+{
+    printf("\nSerie S1C While:\n");
+    int i = 0;
+    while (i <= llCantidad)
+    {
+        printf("%c ", (i % 2 == 0) ? '+' : '-');
+        i++;
+    }
+    printf("\n");
+}
 
+void llSerieS1CDoWhile(int llCantidad)
+{
+    printf("\nSerie S1C Do While:\n");
+    int i = 0;
+    do
+    {
+        printf("%c ", (i % 2 == 0) ? '+' : '-');
+        i++;
+    } while (i <= llCantidad);
+    printf("\n");
+}
