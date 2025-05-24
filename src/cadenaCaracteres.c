f20d59d1c1fb17b88b3017de53d4f046d55a738e
CristhianA-prog <cr7.jacome.15@gmail.com>
2025-05-24 11:42:21 -0500

    inicio del repositorio

--- src/cadenaCaracteres.c
+++ src/cadenaCaracteres.c
@@ -3,23 +3,53 @@
 #include <ctype.h>
 
 // Función para contar vocales
-int jccontarVocales(char frasejc[]) {
-    int contadorjc = 0;
-    for (int i = 0; frasejc[i] != '\0'; i++) {
-        char letrajc = tolower(frasejc[i]);
-        if (letrajc == 'a' || letrajc == 'e' || letrajc == 'i' || letrajc == 'o' || letrajc == 'u') {
-            contadorjc++;
+int contarVocalesJC(char fraseJC[]) {
+    int contadorJC = 0;
+    for (int i = 0; fraseJC[i] != '\0'; i++) {
+        char letraJC = tolower(fraseJC[i]);
+        if (letraJC == 'a' || letraJC == 'e' || letraJC == 'i' || letraJC == 'o' || letraJC == 'u') {
+            contadorJC++;
         }
     }
-    return contadorjc;
+    return contadorJC;
 }
 
 int main() {
-    char frasejc[] = "hamburguesa";  // Frase fija
-    printf("Frase: %s\n", frasejc);
+    char fraseJC[] = "hamburguesa";  // Frase fija
+    printf("Frase: %s\n", fraseJC);
 
-    int totalVocalesjc = jccontarVocales(frasejc);
-    printf("Tiene %d vocales\n", totalVocalesjc);
+    int totalVocalesJC = JCcontarVocales(fraseJC);
+    printf("Tiene %d vocales\n", totalVocalesJC);
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int contarVocalesJC(char fraseJC[]) {
+    int contadorJC = 0;
+    int iJC = 0;
+    char letraJC;
+
+    do {
+        letraJC = tolower(fraseJC[iJC]);
+        if (letraJC == 'a' || letraJC == 'e' || letraJC == 'i' || letraJC == 'o' || letraJC == 'u') {
+            contadorJC++;
+        }
+        iJC++;
+    } while (fraseJC[iJC] != '\0');
+
+    return contadorJC;
+}
+
+int main() {
+    char fraseJC[] = "hamburguesa";
+    printf("Frase: %s\n", fraseJC);
+
+    int totalVocalesJC = contarVocalesJC(fraseJC);
+    printf("Tiene %d vocales\n", totalVocalesJC);
     return 0;
 }
 
