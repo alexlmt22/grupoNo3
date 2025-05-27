7c966c8e907f1d0f558e2000fe7cb2b0383c3bbd
Juan Jurado <juradodiego771@gmail.com>
2025-05-27 13:41:38 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,7 +10,40 @@
 
 int main()
 {
-    void f1 (niveles);
+  
+    int figura, niveles;
+    printf("Selecciona la figura (1-21 ");
+    scanf("%d", &figura);
+    printf("Introduce el tamaño / niveles: ");
+    scanf("%d", &niveles);
+
+    switch (figura) {
+        case 1: F1(niveles); break;
+        case 2: F2(niveles); break;
+        case 3: F3(niveles); break;
+        case 4: F4(niveles); break;
+        case 5: F5(niveles); break;
+        case 6: F6(niveles); break;
+        case 7: F7(niveles); break;
+        case 8: F8(niveles); break;
+        case 9: F9(niveles); break;
+        case 10: F10(niveles); break;
+        case 11: F11(niveles); break;
+        case 12: F12(niveles); break;
+        case 13: F13(niveles); break;
+        case 14: F14(niveles); break;
+        case 15: F15(niveles); break;
+        case 16: F16(niveles); break;
+        case 17: F17(niveles); break;
+        case 18: F18(niveles); break;
+        case 19: F19(niveles); break;
+        case 20: F20(niveles); break;
+        case 21: F21(niveles); break;
+        default: printf("Figura no válida.\n");
+    }
+
+    return 0;
+
     leaVerPortada();
     leaControladorSerie();
     llControladorArray();
