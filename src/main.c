7733432022613134711b7eac5e5b1221f0fbf5cc
Juan Jurado <juradodiego771@gmail.com>
2025-05-27 13:50:10 -0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -10,37 +10,29 @@
 
 int main()
 {
-  
-    int figura, niveles;
-    printf("Selecciona la figura (1-21 ");
-    scanf("%d", &figura);
-    printf("Introduce el tamaño / niveles: ");
-    scanf("%d", &niveles);
 
-    switch (figura) {
-        case 1: F1(niveles); break;
-        case 2: F2(niveles); break;
-        case 3: F3(niveles); break;
-        case 4: F4(niveles); break;
-        case 5: F5(niveles); break;
-        case 6: F6(niveles); break;
-        case 7: F7(niveles); break;
-        case 8: F8(niveles); break;
-        case 9: F9(niveles); break;
-        case 10: F10(niveles); break;
-        case 11: F11(niveles); break;
-        case 12: F12(niveles); break;
-        case 13: F13(niveles); break;
-        case 14: F14(niveles); break;
-        case 15: F15(niveles); break;
-        case 16: F16(niveles); break;
-        case 17: F17(niveles); break;
-        case 18: F18(niveles); break;
-        case 19: F19(niveles); break;
-        case 20: F20(niveles); break;
-        case 21: F21(niveles); break;
-        default: printf("Figura no válida.\n");
-    }
+    
+        F1(niveles); 
+        F2(niveles); 
+        F3(niveles);
+        F4(niveles); 
+        F5(niveles); 
+        F6(niveles); 
+        F7(niveles); 
+        F8(niveles); 
+        F9(niveles); 
+        F10(niveles); 
+        F11(niveles); 
+        F12(niveles); 
+        F13(niveles); 
+        F14(niveles); 
+        F15(niveles); 
+        F16(niveles); 
+        F17(niveles); 
+        F18(niveles); 
+        F19(niveles); 
+        F20(niveles); 
+        F21(niveles); 
 
 
 
